36d7439e01bffecc4d632f6e45fc990c866c6be1
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-15 22:36:02 +0300

    lab4/Galanin_Pavel : Added report

--- lab4/src/gpi_lab4_task3_option5_csharp/gpi_lab4_task3_option5_csharp/gpi_LibraryMenus.cs
+++ lab4/src/gpi_lab4_task3_option5_csharp/gpi_lab4_task3_option5_csharp/gpi_LibraryMenus.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 partial class gpi_LibraryReader
 {
--- lab4/src/gpi_lab4_task3_option5_csharp/gpi_lab4_task3_option5_csharp/gpi_LibraryReader.cs
+++ lab4/src/gpi_lab4_task3_option5_csharp/gpi_lab4_task3_option5_csharp/gpi_LibraryReader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 partial class gpi_LibraryReader
