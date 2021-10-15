880b2a115f9832fb221f8aa34ab3d8bc6ec2d7a8
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-15 13:12:33 +0300

    lab4/Galanin_Pavel : Added empty project

---
+++ lab4/src/gpi_lab4_task3_option5_csharp/gpi_lab4_task3_option5_csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gpi_lab4_task3_option5_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
