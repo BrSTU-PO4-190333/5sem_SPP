d8d14d65ceadd2e393c96c3481a68ab9d1a2bed8
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-12-02 22:45:54 +0300

    lab7/Galanin_Pavel : Added task1 option5

--- lab7/gpi_spp5_lab7_task1_option5/src/com/example/GPI_Main.java
+++ lab7/gpi_spp5_lab7_task1_option5/src/com/example/GPI_Main.java
@@ -3,5 +3,7 @@ package com.example;
 public class GPI_Main {
 	public static void main(String[] args) {
 		System.out.println("Привет, мир!");
+		GPI_SimpleGUI obj = new GPI_SimpleGUI();
+		obj.gpi_run();
 	}
 }
