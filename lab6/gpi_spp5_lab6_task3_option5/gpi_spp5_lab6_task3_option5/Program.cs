d7ce5c4b85d078fdebd4895b8b54d9daab116e5b
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-12-03 01:04:15 +0300

    lab6/Galanin_Pavel : Added empty project

---
+++ lab6/gpi_spp5_lab6_task3_option5/gpi_spp5_lab6_task3_option5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gpi_spp5_lab6_task3_option5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
